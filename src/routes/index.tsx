a754f712aee26c09bab70e928a6d4815b605e7b1
Tomasz Gil <tomaszgil.dev@gmail.com>
2025-08-30 12:27:13 +0200

    Add project rules and mcp servers

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from 'convex/react'
 import react from '@/assets/react.svg'
 import { Button } from '@/components/ui/button'
-import { api } from '../../convex/_generated/api'
+import { api } from '~/convex/_generated/api'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -14,7 +14,7 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
-      <Button>Click me</Button>
+      <Button>Click me here</Button>
 
       <img src={react} alt="React Logo" />
       {products?.map(({ _id, name, unit }) => (
